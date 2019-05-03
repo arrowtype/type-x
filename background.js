9013ca7aad9f9183b1abd8de267d8c96c35bb574
Roel Nieskens <roel@pixelambacht.nl>
2019-05-03 15:14:03 +0200

    [WiP] Playing with quick toggle of font

--- background.js
+++ background.js
@@ -1,31 +1,60 @@
 // Recursive
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({
-    "fontActivated": true
-  });
+    chrome.storage.sync.set({
+        "fontActivated": true
+    });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, { status }, { active }) => {
-  if (active && status === "loading") {
-    chrome.storage.sync.get(
-      "fontActivated", ({ fontActivated }) => {
-        toggle(fontActivated);
-      }
-    );
-  }
+    if (active && status === "loading") {
+        chrome.storage.sync.get(
+            "fontActivated", ({ fontActivated }) => {
+                toggle(fontActivated);
+            }
+        );
+    }
 });
 
 function toggle(fontActivated) {
-  const color = fontActivated ? "hotpink" : "black";
+    // The value "#" results in an invalid `font-family` rule,
+    // restoring the site's original `font-family` rule.
+    // Toggling CSS variables is faster than addding/removing classes!
+    const swapAction = fontActivated ? "hankypankyschnitzelhosen" : "*";
+    // const swapAction = fontActivated ? "add" : "remove";
+    console.log(swapAction);
 
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  }, tabs => {
-    chrome.tabs.executeScript(
-      tabs[0].id, {
-        code: `document.body.style.backgroundColor="${color}"`
-      });
-  });
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tabs => {
+        // TODO: Inject only once per page
+        // It's only injected again when toggling the extension
+        // on and off, and doesn't really fudge anyting up, so
+        // I guess this is low prio.
+        chrome.tabs.insertCSS(tabs[0].id, {
+            file: "css/apply.css",
+            runAt: "document_start"
+        });
+
+
+        // Swapping classes is really slow. So let's use
+        // a CSS variable
+        // TODO: how to unset CSS variable so it doesn't
+        // overwrite the site's font-family?
+        // chrome.tabs.executeScript(
+        //     tabs[0].id, {
+        //         // code: `document.body.classList.${swapAction}("hankypankyschnitzelhosen-disabled");`,
+        //         code: `document.documentElement.style.setProperty("--hanky", '${swapAction}');`,
+        //         runAt: "document_start"
+        //     });
+
+
+
+
+
+        // TODO: upon turning the font back on again, it's requested
+        // and returns a 200 after ~ 60ms. Why can't it be cached or
+        // served quicker?
+    });
 }
--- popup.js
+++ popup.js
@@ -3,19 +3,19 @@
 let activateFonts = document.getElementById('activateFonts');
 
 activateFonts.onclick = element => {
-  chrome.storage.sync.get(
-    "fontActivated", ({ fontActivated }) => {
-      chrome.storage.sync.set({
-        "fontActivated": !fontActivated
-      }, () => {
-        // TODO: properly show state of fontActivated
-        // instead of sticking it in the button text
-        activateFonts.innerText = !fontActivated;
+    chrome.storage.sync.get(
+        "fontActivated", ({ fontActivated }) => {
+            chrome.storage.sync.set({
+                "fontActivated": !fontActivated
+            }, () => {
+                // TODO: properly show state of fontActivated
+                // instead of sticking it in the button text
+                activateFonts.innerText = !fontActivated;
 
-        chrome.runtime.getBackgroundPage(backgroundPage => {
-          backgroundPage.toggle(!fontActivated);
-        });
-      });
-    }
-  );
+                chrome.runtime.getBackgroundPage(backgroundPage => {
+                    backgroundPage.toggle(!fontActivated);
+                });
+            });
+        }
+    );
 };
