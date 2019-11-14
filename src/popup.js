8d48122890f0bfdceb0d6e614390c8fec66f8633
Roel Nieskens <roel@pixelambacht.nl>
2019-11-14 16:09:18 +0100

    Move to non-Babel build with Webpack + Fontkit-new

--- src/popup.js
+++ src/popup.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import fontkit from "fontkit";
+import fontkit from "fontkit-next";
 import blobToBuffer from "blob-to-buffer";
 
 const activateFonts = document.querySelector("#activateFonts");
