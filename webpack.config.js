5cde16745eb8e14bb03b27bb3023bd1647e77580
Simon Cozens <simon@simon-cozens.org>
2025-09-02 09:52:50 +0100

    Update webpack and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,13 @@ module.exports = {
 		filename: "popup.js",
 		chunkFilename: "[name].bundle.js"
 	},
-	node: {
-		fs: "empty"
-	}
+    resolve: {
+        fallback: {
+            fs: false,
+            "buffer": require.resolve("buffer"),
+            "string_decoder": require.resolve("string_decoder/"),
+            "stream": false,
+            "util": require.resolve("util")
+        }
+    }
 };
