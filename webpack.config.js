a2454a88f1c56ac7b06698ab66c7edd8bafaaa2a
Simon Cozens <simon@simon-cozens.org>
2025-09-02 16:05:08 +0100

    Use copy-webpack-plugin to set up dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
 	mode: "production",
 	performance: {
@@ -6,15 +8,27 @@ module.exports = {
 	entry: "./src/popup.js",
 	output: {
 		filename: "popup.js",
-		chunkFilename: "[name].bundle.js"
+		chunkFilename: "[name].bundle.js",
+		clean: true
 	},
-    resolve: {
-        fallback: {
-            fs: false,
-            "buffer": require.resolve("buffer"),
-            "string_decoder": require.resolve("string_decoder/"),
-            "stream": false,
-            "util": require.resolve("util")
-        }
-    }
+	resolve: {
+		fallback: {
+			fs: false,
+			buffer: require.resolve("buffer"),
+			string_decoder: require.resolve("string_decoder/"),
+			stream: false,
+			util: require.resolve("util")
+		}
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: "src/icons", to: "icons/" },
+				{ from: "src/fonts", to: "fonts/" },
+				{ from: "src/*.html", to: "[name][ext]" },
+				{ from: "src/*.css", to: "[name][ext]" },
+				{ from: "src/manifest.json", to: "manifest.json" }
+			]
+		})
+	]
 };
